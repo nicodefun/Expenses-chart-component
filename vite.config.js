8cd53c44ba9183f1bd71031b435d3e6f3c9cd85c
Yonnie Yang <nicodefunyang@gmail.com>
2023-05-29 20:04:32 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/Expenses-chart-component/'
 })
+
+// https://github.com/nicodefun/Expenses-chart-component.git
